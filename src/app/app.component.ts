c6a3e089663c7633bee8969808c4781e25538241
deepan <deepanappu1999@gmail.com>
2024-04-23 20:00:45 +0530

    adding dark and light theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { ThemeService } from './services/theme.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+interface City {
+  name: string;
+  code: string;
+}
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ButtonModule,DropdownModule,FormsModule,],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'nextPlay';
+
+  constructor(private themeServices: ThemeService) { }
+  isExpanding = false;
+  
+
+  cities: City[] | undefined;
+
+  selectedCity: City | undefined;
+
+  theme: any
+
+  ngOnInit() {
+    this.cities = [
+      { name: 'saga-green', code: 'NY' },
+      { name: 'md-dark-indigo', code: 'RM' }
+    ];
+  }
+
+  getTheme() {
+    this.theme = this.selectedCity?.name
+    this.themeServices.switchTheme(this.theme)
+  }
+
 }
