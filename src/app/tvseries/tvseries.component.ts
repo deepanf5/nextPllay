43f4c0a08ef831bb35c73fd591a103233ebf3601
deepan <deepanappu1999@gmail.com>
2024-05-31 20:54:47 +0530

    adding routes

---
+++ src/app/tvseries/tvseries.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tvseries',
+  standalone: true,
+  imports: [],
+  templateUrl: './tvseries.component.html',
+  styleUrl: './tvseries.component.scss'
+})
+export class TvseriesComponent {
+
+}
