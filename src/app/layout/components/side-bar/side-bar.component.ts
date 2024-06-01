34ff3745756fa944e0ee26027e090d67d3ac9a8c
deepan <deepanappu1999@gmail.com>
2024-06-01 07:24:02 +0530

    defining route

--- src/app/layout/components/side-bar/side-bar.component.ts
+++ src/app/layout/components/side-bar/side-bar.component.ts
@@ -1,17 +1,45 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { LayoutService } from '../../services/layout.service';
 import { RouterModule } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,JsonPipe],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit {
+  menuitems: MenuItem[] | undefined;
 
   constructor(public layoutService:LayoutService, public el:ElementRef){}
+
+  ngOnInit(): void {
+    this.menuitems = [
+      {
+          label: 'Dash board',
+          routerLink: [''],
+          items: [
+              {
+                  label: 'Movies',
+                  icon: 'pi pi-video',
+                  routerLink: ['movies']
+              },
+              {
+                  label: 'Tv series',
+                  icon: 'pi pi-search',
+                  routerLink: ['tv-series']
+              },
+              {
+                  label: 'Sports',
+                  icon: 'pi pi-search'
+              }
+          ]
+      }
+  ];
+  }
 
 
 }
