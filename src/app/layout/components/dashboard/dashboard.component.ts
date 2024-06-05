fc6061770af2586babb8e22a1768338d6d64191b
deepan <deepanappu1999@gmail.com>
2024-06-05 21:56:29 +0530

    adding p-carousel

--- src/app/layout/components/dashboard/dashboard.component.ts
+++ src/app/layout/components/dashboard/dashboard.component.ts
@@ -12,8 +12,32 @@ export class DashboardComponent implements OnInit {
   
   responsiveOptions:any;
   products:any
-  
+  images = [
+
+
+    {
+    image:"https://plus.unsplash.com/premium_vector-1711987766219-5a62655e2089?bg=FFFFFF&q=80&w=1958&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+    image:"https://images.unsplash.com/photo-1472214103451-9374bd1c798e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+    image:"https://images.unsplash.com/photo-1472214103451-9374bd1c798e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+    image:"https://plus.unsplash.com/premium_vector-1682303472038-719343faa767?bg=FFFFFF&q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+    image:"https://images.unsplash.com/photo-1472214103451-9374bd1c798e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+    image:"https://images.unsplash.com/photo-1472214103451-9374bd1c798e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+  ]
   ngOnInit(): void {
+    
+  
+  
     this.responsiveOptions = [
       {
           breakpoint: '1199px',
@@ -32,13 +56,7 @@ export class DashboardComponent implements OnInit {
       }
   ];
 
-  this.responsiveOptions = [
-    {
-        breakpoint: '1199px',
-        numVisible: 1,
-        numScroll: 1
-    }
-];
+
 
 this.products = [
   {
