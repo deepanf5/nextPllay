77a22195df4f4a052fb9a9595c3cff101e3986a2
deepan <deepanappu1999@gmail.com>
2024-06-19 21:30:41 +0530

    adding video.js

--- src/app/movies/movie-details/movie-details.component.ts
+++ src/app/movies/movie-details/movie-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, effect } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
@@ -8,6 +8,8 @@ import { faPlay } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterModule } from '@angular/router';
 import { SidebarModule } from 'primeng/sidebar';
+
+declare var videojs: any;
 
 @Component({
   selector: 'app-movie-details',
@@ -20,22 +22,37 @@ import { SidebarModule } from 'primeng/sidebar';
     ReactiveFormsModule,
     FontAwesomeModule,
     RouterModule,
-    SidebarModule
+    SidebarModule,
+    
   ],
   templateUrl: './movie-details.component.html',
   styleUrl: './movie-details.component.scss'
 })
-export class MovieDetailsComponent implements OnInit{
+export class MovieDetailsComponent implements OnInit, AfterViewInit, OnDestroy{
   formGroup!: FormGroup;
   value = 3.5;
   playIcon = faPlay;
   movie:any;
-  watchMovie:boolean = false
+  watchMovie:boolean = false;
+  player!: any;
+  @ViewChild('target', {static: true}) target!: ElementRef;
+  videoLink = 'https://vjs.zencdn.net/v/oceans.mp4';
+
+options = {
+  autoplay: false,
+  notSupportedMessage: 'Invalid Video URL', // to change the default message
+
+}
+  // player: videojs.Player;
+  // player: videojs;
 
   constructor(private moviesServices:MoviesService) {
      this.movie =  this.moviesServices.movieDetail();
-     console.log(this.movie.poster)
 
+  }
+  
+  ngAfterViewInit(): void {
+      this.readyVideojsPlayer()
   }
 
   ngOnInit(): void {
@@ -43,6 +60,29 @@ export class MovieDetailsComponent implements OnInit{
       value: new FormControl(4)
   });
 }
+
+
+readyVideojsPlayer() {
+
+  this.player = videojs(this.target.nativeElement, this.options, function () { 
+  
+  });
+
+  this.player.src({
+
+     src: this.videoLink,
+      type: 'video/mp4'
+
+ })
+
+}
+
+ngOnDestroy(): void {
+  if (this.player) {
+    this.player.dispose();
+  }
+}
+
 
 
  
